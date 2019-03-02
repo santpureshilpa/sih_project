90aa0da8ef7344ee6f0de11cd869f666421987ae
vishal aher <vishl@gmail.com>
2019-03-01 23:51:37 -0800

    Upto Add items

--- app/src/main/java/com/example/signup/activities/ItemsList.java
+++ app/src/main/java/com/example/signup/activities/ItemsList.java
@@ -2,10 +2,13 @@ package com.example.signup.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import com.example.signup.R;
 
 public class ItemsList extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
